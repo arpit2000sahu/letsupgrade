e1c0ea310321d00c52869aa9f1d4f324c3c087ea
arpit2000sahu <89405209+arpit2000sahu@users.noreply.github.com>
2022-11-17 21:50:04 +0530

    Create Assignment_2.cpp

---
+++ Assignment_2.cpp
@@ -0,0 +1,19 @@
+//Swaping two user input number
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, temp;
+    cout << "Enter value of a:";
+    cin >> a;
+    cout << "Enter value of b:";
+    cin >> b;
+
+    cout << "Before swapping a = " << a << " and b = " << b << endl;
+    temp = b;
+    b = a;
+    a = temp;
+    cout << "After swapping a = " << a << " and b = " << b << endl;
+}
