eb8b3795fe1dbb3d1cac80886c5d1c51314f5705
arpit2000sahu <89405209+arpit2000sahu@users.noreply.github.com>
2022-11-30 14:01:46 +0530

    Sum and Product of all natural numbers

--- Assignment_4_sum_and_product_using_loop.cpp
+++ Assignment_4_sum_and_product_using_loop.cpp
@@ -1,18 +1,19 @@
-// Sum of natural numbers
+// Sum & Product of natural numbers
 
 #include <iostream>
 using namespace std;
 
 int main() {
-    int a,sum=0,b;
+    int a,sum=0,product=1,b;
     cout<<"Enter natural number: ";
     cin>>b;
     a=b;
     do{
         sum=b+sum;
+        product=b*product;
         b=b-1;
     }while(b>0);
-    cout<<sum<<" is the sum of all natural numbers till "<<a;
+    cout<<sum<<" & "<<product<<" are the sum & product of all natural numbers till "<<a;
     
     return 0;
 }
