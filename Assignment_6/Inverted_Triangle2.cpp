8bea86fdfad333820e1c27efcf27c7c50457ba94
arpit2000sahu <89405209+arpit2000sahu@users.noreply.github.com>
2022-12-06 00:00:31 +0530

    Inverted Triangle 2 Program

---
+++ Assignment_6/Inverted_Triangle2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int n;
+  cout<<"Enter number of rows:";
+  cin>>n;
+  for(int i = n; i >= 1; i--)
+  {
+    for(int j = i; j <n; j++){
+        cout<<" ";
+    }
+    for(int k = i; k >=1; k--)
+    {
+      cout<<"*";
+    }
+    cout<<"\n";
+  }
+  return 0;
+}
